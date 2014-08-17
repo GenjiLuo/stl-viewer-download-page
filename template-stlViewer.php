05f7a69630fb51c40e2b251c42cb520055a09f9a
jerknose <travis@travisbennett.com>
2014-08-16 19:55:39 -0700

    Added template for codame wordpress theme

---
+++ template-stlViewer.php
@@ -0,0 +1,330 @@
+<?php
+/*
+Template Name: STL Viewer / Download Page
+*/
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Codame ModBod3d Viewer</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+		<link rel="stylesheet" id="open-sans-css" href="//fonts.googleapis.com/css?family=Open+Sans%3A300italic%2C400italic%2C600italic%2C300%2C400%2C600&amp;subset=latin%2Clatin-ext&amp;ver=3.9.2" type="text/css" media="all">
+    <link rel="stylesheet" id="dashicons-css" href="http://www.codame.com/wp-includes/css/dashicons.min.css?ver=3.9.2" type="text/css" media="all">
+    <link rel="stylesheet" id="admin-bar-css" href="http://www.codame.com/wp-includes/css/admin-bar.min.css?ver=3.9.2" type="text/css" media="all">
+    <link rel="stylesheet" id="contact-form-7-css" href="http://www.codame.com/wp-content/plugins/contact-form-7/includes/css/styles.css?ver=3.9.1" type="text/css" media="all">
+    <link rel="stylesheet" id="flow-google-fonts-css" href="http://fonts.googleapis.com/css?family=Dosis:400,800,700,600,500,300,200|Open+Sans:400,800,800italic,700italic,700,600italic,600,400italic,300italic,300|Lato:400,700,400italic,900,300italic,300,100,700italic&amp;subset=latin,latin-ext" type="text/css" media="all">
+    <link rel="stylesheet" id="flow-wpml-language-switcher-script-css" href="http://www.codame.com/wp-content/themes/daisho/modules/module-wpml/wpml-language-switcher.css?ver=3.9.2" type="text/css" media="all">
+    <link rel="stylesheet" id="flow-content-slider-style-css" href="http://www.codame.com/wp-content/themes/daisho/modules/shortcode-content-slider/content-slider.css?ver=3.9.2" type="text/css" media="all">
+    <link rel="stylesheet" id="flow-gmap-style-css" href="http://www.codame.com/wp-content/themes/daisho/modules/shortcode-gmap/gmap.css?ver=3.9.2" type="text/css" media="all">
+    <link rel="stylesheet" id="flow-style-css" href="http://www.codame.com/wp-content/themes/daisho-child/style.css?ver=3.9.2" type="text/css" media="all">
+    <link rel="stylesheet" id="flow-fonts-css" href="http://www.codame.com/wp-content/themes/daisho/modules/fonts/fonts.css?ver=3.9.2" type="text/css" media="all">
+    <link rel="stylesheet" id="flow-fontawesome-css" href="http://www.codame.com/wp-content/themes/daisho/modules/fonts/fontawesome/font-awesome.css?ver=3.9.2" type="text/css" media="all">
+    <link rel="stylesheet" id="flow-portfolio-style-css" href="http://www.codame.com/wp-content/themes/daisho/modules/library-isotope/portfolio.css?ver=3.9.2" type="text/css" media="all">
+    <link rel="stylesheet" id="flow-slideshow-style-css" href="http://www.codame.com/wp-content/themes/daisho/modules/library-iscroll4/slideshow.css?ver=3.9.2" type="text/css" media="all">
+    <link rel="stylesheet" id="prism-css" href="http://www.codame.com/wp-content/plugins/prism-wp/libs/prism/themes/prism.css?ver=1.0.0" type="text/css" media="all">
+    <style type="text/css">
+      .nav-menu li a, .menu-item[class^="modernpicrograms-icon-"] > a, .menu-item[class*=" modernpicrograms-icon-"] > a { color: #ffffff; } .info-box .info-box-inner { color: #ffffff; } .site-title:hover { color: #ffffff; } .nav-menu > .current_page_item > a, .nav-menu > .current-menu-item > a { color: #0092d1; } .nav-menu li:hover > a, .nav-menu li a:hover, .nav-menu .menu-item[class^="modernpicrograms-icon-"] > a:before:hover, .menu-item[class*=" modernpicrograms-icon-"] > a:before:hover, .nav-menu .menu-item[class^="modernpicrograms-icon-"]:hover > a:before, .menu-item[class*=" modernpicrograms-icon-"]:hover > a:before { color: #0092d1; } .info-box { background-color: #020202; } .info-box .info-box-inner h1, .info-box .info-box-inner h2, .info-box .info-box-inner h3, .info-box .info-box-inner h4, .info-box .info-box-inner h5, .info-box .info-box-inner h6 { color: #ffffff; } .page-title { color: #000000; font-size: 22px; } .page-description { color: #000000; font-size: 14px; } .site-title { color: #ffffff; }</style><style type="text/css" media="print">#wpadminbar { display:none; }
+    </style>
+    <style type="text/css" media="screen">
+      html { margin-top: 0 !important; }
+      * html body { margin-top: 32px !important; }
+      @media screen and ( max-width: 782px ) {
+        html { margin-top: 46px !important; }
+        * html body { margin-top: 46px !important; }
+      }
+    </style>
+    <style type="text/css" id="custom-css">/*
+      menu
+      */
+      .nav-menu > li:hover > a, .nav-menu > li > a:hover {  color:#0092d1; }
+      .toggled-on .nav-menu li:hover > a, .toggled-on .nav-menu .children a,
+      .toggled-on .nav-menu .current_page_item > a, .toggled-on .nav-menu .current-menu-item > a { color:#0092d1; }
+      .site-footer .widget-title{color:#fff;}
+      .site-footer .widget-title-date{color:#0092D1;}
+      .site-footer .widget-content-title{color:#fff;}
+      .site-footer .widget-content-desc{ color:#fff;}
+      .site-footer .widget-project-link{color:#0092D1;}
+      .navigation a, .paging-navigation a{color:#0092D1;}
+      .tn-grid-container #options a:hover, .tn-grid-container #options a.selected{color:#0092D1;}
+      .sharing-icons a{ color:#7D7B6D; }
+      .sharing-icons a:hover{ color:#0092D1; }
+      .sharing-icons-tooltip:after { color:#0092D1; background-color:#fff; }
+      @media (max-width: 767px) {.site-header .site-navigation .nav-menu .menu-item a { color:#000; }}
+      .site-header .site-navigation .nav-menu .menu-item a:hover, .site-header .site-navigation .nav-menu .menu-item a:active { color:#0092D1; } 
+
+      html, button, input, select, textarea, h1, h2, h3, h4, h5, h6, .single-meta, .single-meta .single-tags a, .comments-title h2, .comment-body, .no-comments, #commentform textarea, .homepage-read-more, .rbp-header h2, .rpp-header h2, input.wpcf7-submit, .ls-container .button, .site-footer, kbd, .info-box .info-box-inner, .info-box h2{ font-family: 'Open-sans', sans-serif; font-weight:100; line-height:normal; letter-spacing:normal; }
+      .tn-grid-container .page-title { font-size:20px; font-family: 'Open-sans', sans-serif; font-weight:100; line-height:normal; letter-spacing:normal; }}
+      .nav-menu > li > a { font-family: 'Open-sans', sans-serif; font-weight:100; line-height:normal; letter-spacing:normal; }
+      .site-footer .widget-title{font-size:20px;}
+      .site-footer .widget-content-title{font-size:14px;}
+      .site-footer .widget-content-desc{font-size:14px; }
+      .site-footer .widget-project-link{font-size:14px;}
+      .tn-grid-container  .navigation .nav-next a, .tn-grid-container .navigation .nav-previous a{ font-size:18px; font-family: 'Open-sans', sans-serif; font-weight:100; line-height:normal; letter-spacing:normal;}
+      .tn-grid-container #options a{ font-size:20px;font-family: 'Open-sans', sans-serif; font-weight:100; line-height:normal; letter-spacing:normal;}
+      .tn-grid-container #options a:hover, .tn-grid-container #options a.selected{ font-size:20px;}
+
+      .site-footer .widget-title {  font-weight:100; }
+      .site-footer .widget-title-date { font-weight:100; }
+      .site-footer .widget-content-title { font-weight:100; }
+      .site-footer .widget-content-desc { font-weight:100; }
+      .project-navigation a, .paging-navigation a { font-weight:100; }
+      .tn-grid-container #options a:hover, .tn-grid-container #options a.selected { font-weight:100; }
+    </style>
+    <style>@-webkit-keyframes poplpfxwyibxwmymsegigxhnpxbilgbqaqhkbjewblk {50% {-webkit-transform:scale(1.2);}100% {-webkit-transform:scale(1);}}@keyframes poplpfxwyibxwmymsegigxhnpxbilgbqaqhkbjewblk {50% {-webkit-transform:scale(1.2);transform:scale(1.2);}100% {-webkit-transform:scale(1);transform:scale(1);}}#lpfxwyibxwmymsegigxhnpxbilgbqaqhkbjewblk{padding:0;margin:0;font:13px Arial,Helvetica;text-transform:none;font-size: 100%;vertical-align:baseline;line-height:normal;color:#fff;border:solid 2px #fff !important;box-sizing:content-box !important;color:#fff !important;display:block !important;height:auto !important;margin:0 !important;opacity:0.9 !important;padding:7px 10px !important;position:fixed !important;visibility:visible !important;width:auto !important;z-index:2147483647 !important;-webkit-border-radius:5px !important;-webkit-box-shadow:0px 0px 20px #000 !important;-webkit-box-sizing:content-box !important;}.lpfxwyibxwmymsegigxhnpxbilgbqaqhkbjewblk-blocked{padding:0;margin:0;font:13px Arial,Helvetica;text-transform:none;font-size: 100%;vertical-align:baseline;line-height:normal;color:#fff;color:#777 !important;display:inline !important;text-decoration:line-through !important;}#lpfxwyibxwmymsegigxhnpxbilgbqaqhkbjewblk br{display:block !important;padding:0;margin:0;font:13px Arial,Helvetica;text-transform:none;font-size: 100%;vertical-align:baseline;line-height:normal;color:#fff;}#lpfxwyibxwmymsegigxhnpxbilgbqaqhkbjewblk span{background:transparent !important;padding:0;margin:0;font:13px Arial,Helvetica;text-transform:none;font-size: 100%;vertical-align:baseline;line-height:normal;color:#fff;}#lpfxwyibxwmymsegigxhnpxbilgbqaqhkbjewblk div{padding:0;margin:0;font:13px Arial,Helvetica;text-transform:none;font-size: 100%;vertical-align:baseline;line-height:normal;color:#fff;border:0 !important;margin:0 !important;padding:0 !important;width:auto !important;letter-spacing:normal !important;font:13px Arial,Helvetica !important;text-align:left !important;text-shadow:none !important;text-transform:none !important;word-spacing:normal !important;}#lpfxwyibxwmymsegigxhnpxbilgbqaqhkbjewblk a{padding:0;margin:0;font:13px Arial,Helvetica;text-transform:none;font-size: 100%;vertical-align:baseline;line-height:normal;color:#fff;font-weight:normal !important;background:none !important;text-decoration:underline !important;color:#fff !important;}a#lpfxwyibxwmymsegigxhnpxbilgbqaqhkbjewblk-gear{padding:0;margin:0;font:13px Arial,Helvetica;text-transform:none;font-size: 100%;vertical-align:baseline;line-height:normal;color:#fff;text-decoration:none !important;position:absolute !important;display:none !important;font-size:20px !important;width:20px !important;height:20px !important;line-height:20px !important;text-align:center !important;background-color:rgba(255,255,255,.8) !important;text-decoration:none !important;color:#330033 !important;}a#lpfxwyibxwmymsegigxhnpxbilgbqaqhkbjewblk-gear:hover{-webkit-animation-name:poplpfxwyibxwmymsegigxhnpxbilgbqaqhkbjewblk !important;animation-name:poplpfxwyibxwmymsegigxhnpxbilgbqaqhkbjewblk !important;-webkit-animation-duration:0.3s !important;animation-duration:0.3s !important;}#lpfxwyibxwmymsegigxhnpxbilgbqaqhkbjewblk:hover #lpfxwyibxwmymsegigxhnpxbilgbqaqhkbjewblk-gear{text-decoration:none !important;display:inline-block !important;}@media print{#lpfxwyibxwmymsegigxhnpxbilgbqaqhkbjewblk{display:none !important;}}</style>
+    <style>
+      body
+      {
+        opacity:1;
+        position:absolute;
+        padding:0;
+        margin:0;
+      }
+      header
+      {
+        background-color:#fff; background-position: center top; background-repeat: repeat-x; background-image: url("http://www.codame.com/wp-content/uploads/2014/05/lite_header.jpg");
+      }
+      .site-header
+      {
+        position:absolute;
+      }
+    </style>
+  </head>	
+	<body onload="start();">
+    <header id="header" class="site-header" role="banner">
+      <div class="site-header-inner">
+        <div class="logo">
+          <div class="logo-inner">
+          <a class="home-link" href="http://www.codame.com/" title="CODAME ART+TECH" rel="home">
+            <img class="site-logo" src="http://www.codame.com/wp-content/uploads/2014/05/LogoUpdate_01.png" alt="CODAME ART+TECH">
+          </a>
+        </div>
+      </div>
+      <nav class="site-navigation" role="navigation">
+        <h3 class="menu-toggle">Menu</h3>
+        <div class="menu-sidebar-nav-container">
+          <ul id="menu-sidebar-nav" class="nav-menu">
+            <li id="menu-item-2459" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2459"><a style="display:none;" href="#" id="downloadLink" download="Download Your 3d Model!">Download Your 3d Model!</a></li>
+          </ul>
+        </div>
+      </nav>
+      </div>
+    </header>
+
+		<script src="/wp-content/themes/daisho-child/js/threejs/three.min.js"></script>
+
+		<script src="/wp-content/themes/daisho-child/js/threejs/loaders/STLLoader.js"></script>
+
+		<script src="/wp-content/themes/daisho-child/js/threejs/Detector.js"></script>
+		<script src="/wp-content/themes/daisho-child/js/threejs/libs/stats.min.js"></script>
+
+		<script>
+
+			if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
+			
+			//default to codame logo stl file
+			var pathtofile = '/wp-content/themes/daisho-child/scans/codame.stl';
+      
+      <?php
+      $id = stripslashes(trim($_GET['id'])).".stl";
+      
+      //get all the files in the scans directory
+      $scans = array();
+      if ($handle = opendir(get_stylesheet_directory().'/scans')) {
+        $i = 0;
+        while (false !== ($file = readdir($handle)))
+        {
+            if (($file != ".") 
+             && ($file != ".."))
+            {
+              $scans[$i] = $file;
+              $i++;
+            }
+        }
+        closedir($handle);
+      
+        $matchId = array_search($id, $scans);
+        if ($matchId !== false)
+        {
+         echo 'var pathtofile = "/wp-content/themes/daisho-child/scans/'.$scans[$matchId].'";';
+        }
+      }
+      ?>
+      
+			var container, stats;
+
+			var camera, cameraTarget, scene, renderer;
+			
+			var modelUrl;
+			var stlmesh;
+			
+			function start(){
+			if (pathtofile !== "/wp-content/themes/daisho-child/scans/codame.stl")
+			{
+			  var dlLink = document.getElementById('downloadLink');
+			  dlLink.href = modelUrl;
+			  dlLink.style.display = 'block';
+			}
+			init(pathtofile);
+			animate();
+			}
+
+      function clearScene() {
+          scene.remove(stlmesh);
+      }
+
+			function init(stlFile) {
+    
+        if (container)
+        {
+          document.body.removeChild( container );
+        }
+        
+        modelUrl = stlFile;
+        document.getElementById('downloadLink').href = modelUrl;
+        
+				container = document.createElement( 'div' );
+				document.body.appendChild( container );
+
+				camera = new THREE.PerspectiveCamera( 35, window.innerWidth / window.innerHeight, 1, 15 );
+				camera.position.set( 3, 0.15, 3 );
+
+				cameraTarget = new THREE.Vector3( 0, -0.25, 0 );
+
+				scene = new THREE.Scene();
+				scene.fog = new THREE.Fog( 0x686868, 2, 15 );
+
+
+				// Ground
+
+				var plane = new THREE.Mesh( new THREE.PlaneGeometry( 40, 40 ), new THREE.MeshPhongMaterial( { ambient: 0x686868, color: 0x000000, specular: 0x101010 } ) );
+				plane.rotation.x = -Math.PI/2;
+				plane.position.y = -1;
+				scene.add( plane );
+
+				plane.receiveShadow = true;
+
+
+				// ASCII file
+
+				var loader = new THREE.STLLoader();
+				loader.addEventListener( 'load', function ( event ) {
+
+					var geometry = event.content;
+					var material = new THREE.MeshPhongMaterial( { ambient: 0x53249e, color: 0x53249e, specular: 0x111111, shininess: 200 } );
+					stlmesh = new THREE.Mesh( geometry, material );
+
+					stlmesh.position.set( 0, - 0.25, 0.6 );
+					stlmesh.rotation.set( 0, - Math.PI / 2, 0 );
+					stlmesh.scale.set( 0.75, 0.75, 0.75 );
+
+					stlmesh.castShadow = true;
+					stlmesh.receiveShadow = true;
+
+					scene.add( stlmesh );
+
+				} );
+				loader.load( modelUrl );
+
+				// Lights
+
+				scene.add( new THREE.AmbientLight( 0x999999 ) );
+
+				addShadowedLight( 1, 1, 1, 0xffffff, 1.35 );
+				addShadowedLight( 0.5, 1, -1, 0xffffff, 1 );
+
+				// renderer
+
+				renderer = new THREE.WebGLRenderer( { antialias: true } );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+
+				renderer.setClearColor( scene.fog.color, 1 );
+
+				renderer.gammaInput = true;
+				renderer.gammaOutput = true;
+
+				renderer.shadowMapEnabled = true;
+				renderer.shadowMapCullFace = THREE.CullFaceBack;
+
+				container.appendChild( renderer.domElement );
+
+				// stats
+
+				//stats = new Stats();
+				//stats.domElement.style.position = 'absolute';
+				//stats.domElement.style.bottom = '0px';
+				//container.appendChild( stats.domElement );
+
+				//
+
+				window.addEventListener( 'resize', onWindowResize, false );
+
+			}
+
+			function addShadowedLight( x, y, z, color, intensity ) {
+
+				var directionalLight = new THREE.DirectionalLight( color, intensity );
+				directionalLight.position.set( x, y, z )
+				scene.add( directionalLight );
+
+				directionalLight.castShadow = true;
+				// directionalLight.shadowCameraVisible = true;
+
+				var d = 1;
+				directionalLight.shadowCameraLeft = -d;
+				directionalLight.shadowCameraRight = d;
+				directionalLight.shadowCameraTop = d;
+				directionalLight.shadowCameraBottom = -d;
+
+				directionalLight.shadowCameraNear = 1;
+				directionalLight.shadowCameraFar = 4;
+
+				directionalLight.shadowMapWidth = 1024;
+				directionalLight.shadowMapHeight = 1024;
+
+				directionalLight.shadowBias = -0.005;
+				directionalLight.shadowDarkness = 0.15;
+
+			}
+
+			function onWindowResize() {
+
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+
+				renderer.setSize( window.innerWidth, window.innerHeight );
+
+			}
+
+			function animate() {
+
+				requestAnimationFrame( animate );
+
+				render();
+				//stats.update();
+
+			}
+
+			function render() {
+
+				var timer = Date.now() * 0.0005;
+
+				//camera.position.x = Math.cos( timer ) * 3;
+				//camera.position.z = Math.sin( timer ) * 3;
+
+        if (stlmesh)
+        {
+          stlmesh.rotation.x = -.05;
+          stlmesh.rotation.z = .05;
+          stlmesh.rotation.y += .02;
+        }
+
+				camera.lookAt( cameraTarget );
+
+				renderer.render( scene, camera );
+
+			}
+
+		</script>
+	</body>
+</html>
